9c4268cc46816fc70ba08af97b3b04c4f9a7e169
Yoshitaro Takaesu useakat@gmail.com <>
2012-12-02 15:10:50 +0900

    automatized to check the maximum of dchi2 using get_dchi2max.sh

--- dchi2_fitting_Eresnl.sh
+++ dchi2_fitting_Eresnl.sh
@@ -6,6 +6,15 @@
 	mv dchi2min_bestfit2ih.dat dchi2min_bestfit2ih_${Eres}_${Eres_nl}.dat >/dev/null 2>&1
 	mv dchi2min_bestfit2nh.dat dchi2min_bestfit2nh_${Eres}_${Eres_nl}.dat >/dev/null 2>&1
     else
-	mv dchi2_error_nh.dat dchi2_error_nh_${Eres}_${Eres_nl}.dat >/dev/null 2>&1
-	mv dchi2_error_ih.dat dchi2_error_ih_${Eres}_${Eres_nl}.dat >/dev/null 2>&1
+	read line < dchi2_error_nh.dat
+	L=`echo $line | cut -d' ' -f 1`
+	dchi2=`echo $line | cut -d' ' -f 2`
+	error=`echo $line | cut -d' ' -f 3`
+	echo ${maxL_nh} ${dchi2max_nh} ${error} > dchi2_error_nh_${Eres}_${Eres_nl}.dat
+
+	read line < dchi2_error_ih.dat
+	L=`echo $line | cut -d' ' -f 1`
+	dchi2=`echo $line | cut -d' ' -f 2`
+	error=`echo $line | cut -d' ' -f 3`
+	echo ${maxL_nh} ${dchi2max_nh} ${error} > dchi2_error_ih_${Eres}_${Eres_nl}.dat
     fi
--- run.sh
+++ run.sh
@@ -238,9 +238,12 @@ if [ ${run_mode} -eq 4 ]; then  # Free analysis
     Lmin=10
     ndiv=100
     source dchi2_fitting_Eresnl.sh
+    source get_dchi2max.sh
+    echo $maxline_nh $maxL_nh $dchi2max_nh > dchi2max_nh_${Eres}_${Eres_nl}.dat
+    echo $maxline_ih $maxL_ih $dchi2max_ih > dchi2max_ih_${Eres}_${Eres_nl}.dat
     ifixL=1
     ifluc=1
-    Lmin=51
+    Lmin=${maxL_nh}
     ndiv=100
     source dchi2_fitting_Eresnl.sh
 
